a6fea3db5a379f5db3e5e558cb66018158a9336a
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2022-02-16 17:59:17 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ExampleOfSelect3.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace ExampleOfSelect3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var list = new List<DropdownList>();
+
+            for (int i = 0; i < 2000000; i++)
+            {
+                list.Add(new DropdownList() { id = i, text = "Test Data-" + i });
+            }
+
+            var serializer = new System.Web.Script.Serialization.JavaScriptSerializer() { MaxJsonLength = Int32.MaxValue };
+            ViewBag.DataAsList = serializer.Serialize(list);
+
+            return View();
+        }
+    }
+}
---
+++ Models/DropdownList.cs
@@ -0,0 +1,8 @@
+﻿namespace ExampleOfSelect3.Models
+{
+    public class DropdownList
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+    }
+}
